e51be5bf986624de21dc79f3a84a53761e3e49c6
affiq23 <affiqem@gmail.com>
2025-07-16 10:33:36 -0500

    added redeploy script update

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,9 +3,6 @@
 PROJECT_DIR=/root/projects/mlh-portfolio
 VENV_DIR=$PROJECT_DIR/venv
 
-echo "killing all existing tmux sessions..."
-tmux kill-server 2>/dev/null
-
 echo "cd into project folder..."
 cd "$PROJECT_DIR" || { echo "Failed to cd into $PROJECT_DIR"; exit 1; }
 
@@ -21,7 +18,7 @@ fi
 echo "activating virtual environment and installing dependencies..."
 source "$VENV_DIR/bin/activate"
 
-
+# Check if pip exists now
 if ! command -v pip &> /dev/null; then
   echo "pip command not found after activating virtualenv"
   exit 1
@@ -29,11 +26,10 @@ fi
 
 pip install -r requirements.txt
 
-echo "starting new tmux session running Flask server..."
-tmux new -d -s flask-server "
-  cd '$PROJECT_DIR'
-  source '$VENV_DIR/bin/activate'
-  flask run --host=0.0.0.0
-"
+echo "restarting myportfolio service..."
+sudo systemctl restart myportfolio
+
+echo "checking service status..."
+sudo systemctl status myportfolio --no-pager
 
 echo "finished!"
