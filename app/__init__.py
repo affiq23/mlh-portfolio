4b5e637cbf6b35db2783e9272dc2acb9804fccdf
affiq23 <affiqem@gmail.com>
2025-08-11 11:26:20 -0500

    chanhge

--- app/__init__.py
+++ app/__init__.py
@@ -174,7 +174,7 @@ def create_app():
         {
             "name": "Space",
             "image": "img/space.jpg",
-            "description": "space",
+            "description": "I like space",
         },
     ]
 
